742ffca0077f084cada4a20783c1d4fadbe8ed1f
sebastianbernalc <sebastianbernalc@udea.edu.co>
2024-08-21 11:43:35 -0500

    Frontend Notes change url api

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import Home from './pages/Home/Home'
 import Login from './pages/Login/Login'
 import Signup from './pages/Signup/Signup'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Navigate } from 'react-router-dom'
 
 const routes = (
   <Router>
     <Routes>
+      <Route path="/" element={<Navigate to="/login" replace />} />
       <Route path="/dashboard" exact element={<Home />} />
       <Route path="/login" exact element={<Login />} />
       <Route path="/signup" exact element={<Signup />} />
